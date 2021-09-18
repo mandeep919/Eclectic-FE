45e033ac43b036f2b313747791a6b4753742962a
MandeepM <maharjanmemandeep@gmail.com>
2021-09-18 14:58:04 +0545

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SearchIcon from "@material-ui/icons/Search";
-import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import { Grid, IconButton } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,72 +16,48 @@ const Header = (props) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-      <a href="/" className="navbar-brand">
-        <img src={Logo} className="logo" alt="Logo" />
-      </a>
-      <form className="navbar-form resp-show form-inline">
-        <div className="input-group search-box">
-          <input
-            type="text"
-            id="search-posts1"
-            className="form-control"
-            placeholder="Search questions here..."
-          />
-          <div className="input-group-append">
-            <span className="input-group-text">
-              <SearchIcon id="search-button" className="" />
-            </span>
-          </div>
+      <div className="nav-inner">
+        <div className="logo-wrap">
+          <a href="/" className="navbar-brand">
+            <img src={Logo} className="logo" alt="Logo" />
+          </a>
         </div>
-      </form>
-      <button
-        type="button"
-        className="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#navbarCollapse"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div
-        id="navbarCollapse"
-        className="collapse navbar-collapse justify-content-start"
-      >
-        <div className="navbar-nav">
-          <Link to="/study-materials" className="nav-item nav-link">
-            <b>Study Materials</b>
-          </Link>
-        </div>
-        <form className="navbar-form resp-hide form-inline">
+        <form className="navbar-form resp-show form-inline">
           <div className="input-group search-box">
             <input
               type="text"
-              id="search-posts"
+              id="search-posts1"
               className="form-control"
               placeholder="Search questions here..."
             />
-            <div className="input-group-append">
+            <div className="btn-search">
               <span className="input-group-text">
-                <SearchIcon className="" />
+                <SearchIcon id="search-button" className="" />
               </span>
             </div>
           </div>
         </form>
+
         <div className="navbar-nav ml-auto action-buttons">
           <div className="notification-icon-wrap">
             <button className="btn-update-password">
               <NotificationsActiveIcon />
             </button>
-            <div className="">
-              <div>
-                <h5 className="mb-3">
-                  <b>Notifications</b>
-                </h5>
-                <li className="notifscore mb-2"></li>
+            {user.notification ? (
+              <div className="">
+                <div>
+                  <h5 className="mb-3">
+                    <b>Notifications</b>
+                  </h5>
+                  <li className="notifscore mb-2">Aman liked your picture</li>
+                  <li className="notifscore mb-2">Aman liked your picture</li>
+                </div>
               </div>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
-          {!user ? (
+          {user ? (
             <>
               <a href="/login" className="nav-link">
                 <b>Login</b>
